fcc3ba9b71f8b0c7086028483aab027e01f05fd6
adam <bcuz@users.noreply.github.com>
2019-07-10 22:55:58 -0400

    change port number

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const server = require('./server.js');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => {
     console.log(`Listening to localhost: ${PORT}`);
