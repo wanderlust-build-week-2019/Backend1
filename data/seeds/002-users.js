39b18673a782dc229a1b79de89e8a38bbec6ec5d
adam <bcuz@users.noreply.github.com>
2019-07-14 15:18:57 -0400

    bcrypt seeded passwords

--- data/seeds/002-users.js
+++ data/seeds/002-users.js
@@ -1,9 +1,14 @@
 // ./seeds/001-roles.js
+const bcrypt = require('bcryptjs');
+
 exports.seed = function(knex, Promise) {
   // the 00-cleanup.js seed already deleted all records
   // we just worry about seeding records in all other seeds
+
+  // password for seeded users is the word 'in'
+  const hash = bcrypt.hashSync('in', 10);
   return knex('users').insert([
-    { username: 'guide', password: 'in', role_id: 1 },
-    { username: 'user', password: 'in', role_id: 2 },
+    { username: 'guide', password: hash, role_id: 1 },
+    { username: 'user', password: hash, role_id: 2 },
   ]);
 };
