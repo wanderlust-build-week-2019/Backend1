0ea150dc31e053f97ee9855a0e7d1eb40a45456a
adam <bcuz@users.noreply.github.com>
2019-07-20 14:06:39 -0400

    delete unnecessary code

--- api/users/usersRouter.js
+++ api/users/usersRouter.js
@@ -15,20 +15,4 @@ router.get('/', async (req, res) => {
   
 });
 
-router.get('/:id', (req, res) => {
-
-});
-
-router.post('/', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
 module.exports = router;
